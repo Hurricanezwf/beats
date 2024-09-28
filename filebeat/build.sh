a68370760eb693a475324d99593fa4c332ed7298
zhouwenfeng <zwf1094646850@gmail.com>
2024-09-28 11:15:18 +0800

    chore: update build.sh

--- filebeat/build.sh
+++ filebeat/build.sh
@@ -12,4 +12,5 @@ rm -f ./filebeat
 go build -ldflags "-s -w"
 
 IMAGE_TAG=8.14.4-hurricanezwf0.2.3
-docker build -f ./Dockerfile -t harbor.123u.com/public/filebeat:${IMAGE_TAG} .
+docker build -f ./Dockerfile -t ops-huanle.tencentcloudcr.com/public/filebeat:${IMAGE_TAG} .
+docker push ops-huanle.tencentcloudcr.com/public/filebeat:${IMAGE_TAG}
